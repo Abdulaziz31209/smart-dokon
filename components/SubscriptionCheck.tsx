1eba1ec8fa66cd01540fbdca34e31b5f9bd89e6a
Abdulaziz <h4ck3r0312@gmail.com>
2026-02-24 20:52:47 +0500

    fix: subscription bypass

---
+++ components/SubscriptionCheck.tsx
@@ -0,0 +1,7 @@
+'use client'
+import React from 'react'
+
+export default function SubscriptionCheck({ children }: { children: React.ReactNode }) {
+  // Bu kod obuna tekshiruvini butunlay chetlab o'tadi
+  return <>{children}</>
+}
